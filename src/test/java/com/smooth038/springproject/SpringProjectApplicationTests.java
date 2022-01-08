6858e48d4ef6edc352f2056bc5bd87f877eb511b
smooth038 <simon.lariviere.pianiste@gmail.com>
2022-01-08 13:43:31 -0500

    FIRST

---
+++ src/test/java/com/smooth038/springproject/SpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smooth038.springproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
