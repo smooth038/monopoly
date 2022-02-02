52689297d8ee30042b1a227f579867410b9b24e9
smooth038 <simon.lariviere.pianiste@gmail.com>
2022-02-02 13:52:10 -0500

    New Game Modal almost done

--- src/main/frontend/src/components/Board.tsx
+++ src/main/frontend/src/components/Board.tsx
@@ -1,25 +1,25 @@
-import styled from 'styled-components';
-import boardImage from '../assets/board-high-res.jpg'
+import styled from "styled-components";
+import boardImage from "assets/board-high-res.jpg";
 
 export interface BoardProps {
   frameHeight: number;
 }
 
-export const Board: React.FC<BoardProps>  = (props: BoardProps) => {
-  return(
+export const Board: React.FC<BoardProps> = (props: BoardProps) => {
+  return (
     <StyledBoard frameHeight={props.frameHeight}>
       <div className="lighting" />
     </StyledBoard>
   );
-}
+};
 
-const StyledBoard = styled.div<{frameHeight: number}>`
+const StyledBoard = styled.div<{ frameHeight: number }>`
   --board-size: ${(props) => (0.95 * props.frameHeight).toString() + "px"};
   border: 3px solid black;
   border-radius: 7px;
   text-align: center;
   box-sizing: border-box;
-  width: var(--board-size); 
+  width: var(--board-size);
   height: var(--board-size);
   margin: 0;
   background-image: url(${boardImage});
@@ -28,8 +28,12 @@ const StyledBoard = styled.div<{frameHeight: number}>`
   filter: drop-shadow(3px 2px 1px black);
 
   @keyframes fromRight {
-      0% {left: 100vw }
-      100% {left: 0px;}
+    0% {
+      left: 100vw;
+    }
+    100% {
+      left: 0px;
+    }
   }
 
   position: relative;
@@ -38,10 +42,11 @@ const StyledBoard = styled.div<{frameHeight: number}>`
   animation-duration: 0.8s;
   animation-timing-function: ease;
   animation-iteration-count: 1;
- 
+
   .lighting {
     position: absolute;
-    width: 100%; height: 100%;
+    width: 100%;
+    height: 100%;
     background-image: linear-gradient(-210deg, #fff2, #fff0);
   }
 `;
--- src/main/frontend/src/components/NewGameModal.tsx
+++ src/main/frontend/src/components/NewGameModal.tsx
@@ -1,36 +1,106 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import styled from "styled-components";
+import bootTokenImg from "assets/tokens/250x250/boot.png";
+import carTokenImg from "assets/tokens/250x250/car.png";
+import catTokenImg from "assets/tokens/250x250/cat.png";
+import dogTokenImg from "assets/tokens/250x250/dog.png";
+import duckTokenImg from "assets/tokens/250x250/duck.png";
+import hatTokenImg from "assets/tokens/250x250/hat.png";
+import ironTokenImg from "assets/tokens/250x250/iron.png";
+import penguinTokenImg from "assets/tokens/250x250/penguin.png";
+import shipTokenImg from "assets/tokens/250x250/ship.png";
+import thimbleTokenImg from "assets/tokens/250x250/thimble.png";
+import t_rexTokenImg from "assets/tokens/250x250/t_rex.png";
+
+export enum Tokens {
+  BOOT = "boot",
+  CAR = "car",
+  CAT = "cat",
+  DOG = "dog",
+  DUCK = "duck",
+  HAT = "hat",
+  IRON = "iron",
+  PENGUIN = "penguin",
+  SHIP = "ship",
+  THIMBLE = "thimble",
+  T_REX = "t_rex",
+}
+
+const tokenImages = new Map([
+  [Tokens.BOOT, bootTokenImg],
+  [Tokens.CAR, carTokenImg],
+  [Tokens.CAT, catTokenImg],
+  [Tokens.DOG, dogTokenImg],
+  [Tokens.DUCK, duckTokenImg],
+  [Tokens.HAT, hatTokenImg],
+  [Tokens.IRON, ironTokenImg],
+  [Tokens.PENGUIN, penguinTokenImg],
+  [Tokens.SHIP, shipTokenImg],
+  [Tokens.THIMBLE, thimbleTokenImg],
+  [Tokens.T_REX, t_rexTokenImg],
+]);
 
 export const NewGameModal = () => {
   const [numberOfPlayers, setNumberOfPlayers] = useState(2);
-  const [players, setPlayers] = useState<Array<[string, Tokens]>>();
-
-  enum Tokens {
-    BOOT = "Boot",
-    CAT = "Cat",
-    DUCK = "Duck",
-    HAT = "Hat",
-    IRON = "Iron",
-    PENGUIN = "Penguin",
-    SHIP = "Ship",
-    THIMBLE = "Thimble",
-    T_REX = "T-Rex",
-  }
+  const [players, setPlayers] = useState<
+    Array<{ name: string; token: Tokens }>
+  >([
+    { name: "", token: Tokens.BOOT },
+    { name: "", token: Tokens.CAR },
+  ]);
 
   const handleNumberOfPlayersChange = (
     event: React.ChangeEvent<HTMLInputElement>
   ) => {
     const inputValue = event.target.value;
     if (inputValue) {
-      const intValue = parseInt(inputValue);
-      if (intValue < 2) {
-        setNumberOfPlayers(2);
-      } else if (intValue > 8) {
-        setNumberOfPlayers(8);
-      } else {
-        setNumberOfPlayers(intValue);
+      let newNumberOfPlayers = parseInt(inputValue);
+      if (newNumberOfPlayers < 2) {
+        newNumberOfPlayers = 2;
+      } else if (newNumberOfPlayers > 8) {
+        newNumberOfPlayers = 8;
+      }
+      if (newNumberOfPlayers !== numberOfPlayers) {
+        const newPlayers = [...players];
+        if (newNumberOfPlayers > numberOfPlayers) {
+          const numberOfPlayersToAdd = newNumberOfPlayers - numberOfPlayers;
+          for (let i = 1; i <= numberOfPlayersToAdd; i++) {
+            newPlayers.push({
+              name: "",
+              token: Object.values(Tokens)[i + numberOfPlayers],
+            });
+          }
+        } else if (newNumberOfPlayers < numberOfPlayers) {
+          const numberOfPlayersToRemove = numberOfPlayers - newNumberOfPlayers;
+          for (let i = 1; i <= numberOfPlayersToRemove; i++) {
+            newPlayers.pop();
+          }
+        }
+        setPlayers(newPlayers);
+        setNumberOfPlayers(newNumberOfPlayers);
       }
     }
+  };
+
+  const handlePlayerNameChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    playerIndex: number
+  ) => {
+    const newPlayers = [...players];
+    newPlayers[playerIndex].name = event.target.value;
+    setPlayers(newPlayers);
+  };
+
+  const handlePlayerTokenChange = (playerIndex: number, tokenIndex: number) => {
+    const newPlayers = [...players];
+    newPlayers[playerIndex].token = Object.values(Tokens)[tokenIndex];
+    setPlayers(newPlayers);
+  };
+
+  const handleNewGameButtonClick = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    alert(players);
   };
 
   return (
@@ -38,32 +108,57 @@ export const NewGameModal = () => {
       <h1>New Game</h1>
       <form>
         <StyledRow>
-          <label>
-            Number of players:&nbsp;&nbsp;
-            <input
-              type="number"
-              onChange={handleNumberOfPlayersChange}
-              value={numberOfPlayers}
-            />
-          </label>
+          <StyledNumberOfPlayers>
+            <label>
+              Number of players:&nbsp;&nbsp;
+              <input
+                type="number"
+                onChange={handleNumberOfPlayersChange}
+                value={numberOfPlayers}
+              />
+            </label>
+          </StyledNumberOfPlayers>
         </StyledRow>
         {Array.from({ length: numberOfPlayers }).map((player, playerIndex) => (
-          <StyledRow>
+          <StyledRow key={playerIndex}>
             <StyledPlayerEntry>
               <StyledInline>
                 <StyledPlayerName>
-                  <input type="text" />
+                  <input
+                    type="text"
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                      handlePlayerNameChange(event, playerIndex)
+                    }
+                  />
                 </StyledPlayerName>
                 <StyledPlayerToken>
-                  {Object.entries(Tokens).map((token, tokenIndex) => (
-                    <label>
-                      {token[1]}
+                  {Object.values(Tokens).map((token, tokenIndex) => (
+                    <span
+                      key={playerIndex.toString() + "-" + tokenIndex.toString()}
+                    >
                       <input
                         type="radio"
+                        id={"player" + playerIndex + "-token"}
                         name={"player" + playerIndex + "-token"}
-                        value={token[0]}
+                        value={token}
+                        checked={
+                          (!players[playerIndex] &&
+                            tokenIndex === playerIndex) ||
+                          players[playerIndex]?.token === token
+                        }
+                        readOnly
                       />
-                    </label>
+                      <label
+                        htmlFor={"player" + playerIndex + "-token"}
+                        onClick={(
+                          event: React.MouseEvent<HTMLLabelElement>
+                        ) => {
+                          handlePlayerTokenChange(playerIndex, tokenIndex);
+                        }}
+                      >
+                        <img src={tokenImages.get(token)} alt={token} />
+                      </label>
+                    </span>
                   ))}
                 </StyledPlayerToken>
               </StyledInline>
@@ -71,7 +166,9 @@ export const NewGameModal = () => {
           </StyledRow>
         ))}
         <StyledFormButtons>
-          <button type="button">Start Game</button>
+          <button type="button" onClick={handleNewGameButtonClick}>
+            Start Game
+          </button>
         </StyledFormButtons>
       </form>
     </StyledNewGameModal>
@@ -89,16 +186,13 @@ const StyledNewGameModal = styled.div`
   border-radius: 10px;
   filter: drop-shadow(3px 2px 2px #000);
   padding: 10px 20px;
-  width: 800px;
   color: white;
   background-image: linear-gradient(-210deg, #fff4, #fff0);
 
   @keyframes fromLeft {
-    0% {
+    from {
       left: -100vw;
-    }
-    100% {
-      left: 0px;
+      opacity: 0;
     }
   }
 
@@ -107,7 +201,14 @@ const StyledNewGameModal = styled.div`
 `;
 
 const StyledRow = styled.div`
-  padding: 10px 0px;
+  padding: 15px 0px;
+`;
+
+const StyledNumberOfPlayers = styled.div`
+  input {
+    height: 1.5em;
+    width: 3em;
+  }
 `;
 const StyledPlayerEntry = styled.div`
   display: flex;
@@ -138,9 +239,49 @@ const StyledPlayerName = styled.div`
 
 const StyledPlayerToken = styled.div`
   margin-left: 8px;
+  white-space: nowrap;
+  input[type="radio"] {
+    opacity: 0;
+    position: fixed;
+    width: 0;
+  }
+  label {
+    display: inline-block;
+    margin: 0px 2px;
+    padding: 0px 3px;
+    font-size: 16px;
+    border: 1px solid transparent;
+    border-radius: 4px;
+  }
+  input[type="radio"]:checked + label {
+    border: 1px solid #4c4;
+    box-shadow: 0 0 0.5em 0 #4c4, inset 0 0 0.5em 0 #4c4;
+  }
+  label:hover {
+    background-color: #dfd3;
+  }
+  img {
+    width: 2.5em;
+    height: 2.5em;
+  }
 `;
 
 const StyledFormButtons = styled.div`
   display: flex;
   justify-content: center;
+  padding: 15px 0px;
+  button {
+    font-size: 18px;
+    padding: 8px 32px;
+    background-color: transparent;
+    border: 2px solid white;
+    box-shadow: 0 0 0.5em 0 white, inset 0 0 0.5em 0 white;
+    color: white;
+    :hover {
+      background-color: #dfd3;
+    }
+    :active {
+      background-color: #4c4;
+    }
+  }
 `;
